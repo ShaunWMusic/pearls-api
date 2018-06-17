8b5236cfe3cf99c38b471302059d56f29ae6dd8f
shaunwmusic <shaunwmusic@gmail.com>
2018-06-17 07:24:55 -0500

    stripe beginnings

---
+++ app/resources/customer_resource.rb
@@ -0,0 +1,4 @@
+class CustomerResource < JSONAPI::Resource
+  attributes :account_balance, :business_vat_id, :coupon, :default_source, :description, :email, :invoice_prefix, :metadata, :shipping, :source
+
+end
