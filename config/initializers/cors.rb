21e9d2276214b099779ad415f6ef1f8c7160b189
shaunwmusic <shaunwmusic@gmail.com>
2018-06-24 14:03:34 -0500

    update url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'pearlsbeardoil.com'
+    origins 'www.pearlsbeardoil.com'
 
     resource '*',
       headers: :any,
