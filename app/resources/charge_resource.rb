3aadab01d42cd8985e320045a7ca5aacba5933bd
Shaun Willis <shaunwmusic@gmail.com>
2019-09-20 07:20:11 -0500

    add beard oil samples

--- app/resources/charge_resource.rb
+++ app/resources/charge_resource.rb
@@ -4,8 +4,13 @@ class ChargeResource < JSONAPI::Resource
   before_save do
      # Token is created using Checkout or Elements!
         # Get the payment token ID submitted by the form:
-        Stripe::Charge.create( :description => description, :source => source, :receipt_email => receipt_email, :amount => '1399', :currency => 'usd') if @model.new_record?
+      if @model.description == "Beard-Sample"
+       Stripe::Charge.create( :description => description, :source => source, :receipt_email => receipt_email, :amount => '299', :currency => 'usd') 
+       @model.amount = '2.99'
+      else 
+       Stripe::Charge.create( :description => description, :source => source, :receipt_email => receipt_email, :amount => '1399', :currency => 'usd') if @model.new_record?
        @model.amount = '13.99'
+      end
        @model.currency = 'usd'
        sendtrackingnumber = @model if @model.tracking_number?
        mail = TrackingNumberMailer.Tracking_number(sendtrackingnumber) if @model.tracking_number?
